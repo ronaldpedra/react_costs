26730d8f9bfef30ee19bf898dbf2a00c213c45cb
Ronald Pedra <ronaldpedra@gmail.com>
2024-06-08 14:50:56 -0300

    Pages layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import Home from "./components/pages/Home";
+import NewProject from "./components/pages/NewProject";
+import Container from "./components/layout/Container";
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <Link to='/'>Home</Link>
+        <Link to='/contact'>Contato</Link>
+        <Link to='/company'>Empresa</Link>
+        <Link to='/newproject'>Novo Projeto</Link>
+      </ul>
+      <Container customClass='min_height'>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/company" element={<Company />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/newproject" element={<NewProject />}></Route>
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
   );
 }
 
---
+++ src/components/pages/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            <h1>Página Inicial</h1>
+        </div>
+    )
+}
+
+export default Home
