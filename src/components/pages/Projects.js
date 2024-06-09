4ee91ad87a43ee5f7520a9b6a496973dd6ce8f5e
Ronald Pedra <ronaldpedra@gmail.com>
2024-06-09 14:14:44 -0300

    New features

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,16 +1,37 @@
 import { useLocation } from "react-router-dom"
+import { useState, useEffect } from "react"
+
 import Message from "../layout/Message"
 import Container from "../layout/Container"
 import LinkButton from "../layout/LinkButton"
 import styles from "./Projects.module.css"
+import ProjectCard from "../project/ProjectCard"
 
 function Projects() {
+
+    const [projects, setProjects] = useState([])
 
     const location = useLocation()
     let message = ''
     if (location.state) {
         message = location.state.message
     }
+
+    useEffect(() => {
+
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data)
+                setProjects(data)
+            })
+            .catch((err) => console.log(err))
+    }, [])
 
     return (
         <div className={styles.project_container}>
@@ -24,7 +45,16 @@ function Projects() {
             )}
 
             <Container customClass='start'>
-                <p>Projetos...</p>
+                {projects.length > 0 &&
+                    projects.map((project) => (
+                        <ProjectCard
+                            id={project.id}
+                            name={project.name}
+                            budget={project.budget}
+                            category={project.category.name}
+                            key={project.id}
+                        />
+                    ))}
             </Container>
         </div>
     )
