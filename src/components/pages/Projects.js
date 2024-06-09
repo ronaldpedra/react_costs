3d11be63fd4dca793e485a340d23f05da5b511dd
Ronald Pedra <ronaldpedra@gmail.com>
2024-06-09 14:47:49 -0300

    Delete implemented

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,15 +1,20 @@
 import { useLocation } from "react-router-dom"
 import { useState, useEffect } from "react"
 
-import Message from "../layout/Message"
 import Container from "../layout/Container"
+import Message from "../layout/Message"
+import Loading from "../layout/Loading"
 import LinkButton from "../layout/LinkButton"
-import styles from "./Projects.module.css"
 import ProjectCard from "../project/ProjectCard"
+
+import styles from "./Projects.module.css"
+
 
 function Projects() {
 
     const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage]  = useState('')
 
     const location = useLocation()
     let message = ''
@@ -19,19 +24,39 @@ function Projects() {
 
     useEffect(() => {
 
-        fetch('http://localhost:5000/projects', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-            .then(resp => resp.json())
-            .then(data => {
-                console.log(data)
-                setProjects(data)
+        setTimeout(() => {
+            fetch('http://localhost:5000/projects', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
             })
-            .catch((err) => console.log(err))
+                .then(resp => resp.json())
+                .then(data => {
+                    console.log(data)
+                    setProjects(data)
+                    setRemoveLoading(true)
+                })
+                .catch((err) => console.log(err))
+        }, 1000)
     }, [])
+
+    function removeProject(id) {
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(resp => resp.json())
+        .then(() => {
+            setProjects(projects.filter((project) => project.id !== id))
+            // message
+            setProjectMessage('Projeto removido com sucesso!')
+        })
+        .catch(err => console.log(err))
+
+    }
 
     return (
         <div className={styles.project_container}>
@@ -43,6 +68,9 @@ function Projects() {
             {message && (
                 <Message type='success' msg={message} />
             )}
+            {projectMessage && (
+                <Message type='success' msg={projectMessage} />
+            )}
 
             <Container customClass='start'>
                 {projects.length > 0 &&
@@ -53,8 +81,13 @@ function Projects() {
                             budget={project.budget}
                             category={project.category.name}
                             key={project.id}
+                            handleRemove={removeProject}
                         />
                     ))}
+                {!removeLoading && <Loading />}
+                {removeLoading && projects.length === 0 && (
+                    <p>Não há Projetos cadastrados!</p>
+                )}
             </Container>
         </div>
     )
