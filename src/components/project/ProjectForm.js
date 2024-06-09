90c28ee98ef9b37cc88726cae7e66eb09717eaa2
Ronald Pedra <ronaldpedra@gmail.com>
2024-06-09 00:12:27 -0300

    New features

---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,28 @@
+import Input from "../form/Input"
+import Select from "../form/Select"
+import SubmitButton from "../form/SubmitButton"
+import styles from "./ProjectForm.module.css"
+
+function ProjectForm({ btnText }) {
+    return (
+        <form className={styles.form}>
+            <Input
+                type={'text'}
+                text={'Nome do Projeto'}
+                name={'name'}
+                placeholder={'Insira o nome do Projeto'} />
+            <Input
+                type={'number'}
+                text={'Orçamento do Projeto'}
+                name={'budget'}
+                placeholder={'Insira o Orçamento Total'} />
+            <Select
+                name={'category_id'}
+                text={'Selecione a Categoria'} />
+            <SubmitButton
+                text={btnText} />
+        </form>
+    )
+}
+
+export default ProjectForm
