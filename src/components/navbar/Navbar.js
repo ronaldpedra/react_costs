324d17c134c973024278b7a644cc4af46d3e669a
Ronald Pedra <ronaldpedra@gmail.com>
2024-06-11 18:36:16 -0300

    Update

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,10 @@
 // Imagens
 import logo from "../../img/costs_logo.png"
+// Folhas de estilo
+import styles from "./css/Navbar.module.css"
 function Navbar() {
     return (
-        <nav>
+        <nav className={styles.navbar_container}>
             <img src={logo} alt="Costs" />
             <ul>
                 <li>Home</li>
