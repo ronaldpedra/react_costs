c1077d99599cdd34b10079eeb0087106ec10c16d
Ronald Pedra <ronaldpedra@gmail.com>
2024-06-11 19:01:51 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+// Recursos de Sistema
+import { Router, Route } from "react-router-dom";
+// Layout
 import Header from "./components/layout/Header";
 
 function App() {
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,17 @@
+// Recursos de Sistema
+import { Link } from "react-router-dom"
 // Layout
 import Container from "../layout/Container"
 // Imagens
 import logo from "../../img/costs_logo.png"
 // Folhas de estilo
 import styles from "./css/Navbar.module.css"
+
 function Navbar() {
     return (
-        <nav>
+        <nav className={styles.navbar_container}>
             <Container>
-                <img src={logo} alt="Costs" />
+                <Link to='/'><img src={logo} alt="Costs" /></Link>
                 <ul>
                     <li>Home</li>
                     <li>Projetos</li>
