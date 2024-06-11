bffda7e15461c0a00c32507d4e9e155896c173c7
ronaldpedra <ronaldpedra@gmail.com>
2024-06-11 15:59:11 -0300

    Update Project.js

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,4 +1,4 @@
-import { parse, v4 as uuidv4 } from "uuid"
+import { v4 as uuidv4 } from "uuid"
 import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react"
 import styles from "./Project.module.css"
