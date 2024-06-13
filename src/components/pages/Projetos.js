35d0887779237d3c174eb2fe617cbd22a16982cf
ronaldpedra <ronaldpedra@gmail.com>
2024-06-13 16:20:13 -0300

    Update

--- src/components/pages/Projetos.js
+++ src/components/pages/Projetos.js
@@ -1,6 +1,20 @@
+import { useLocation } from "react-router-dom"
+import styles from "./css/Projetos.module.css"
+import Message from "../message/Message"
+
 function Projetos() {
+
+    const location = useLocation()
+    let message ={}
+    if (location.state) {
+        message = location.state.message
+    }
+
     return (
         <div>
+            <Message
+            type={message.type}
+            text={message.text} />
             <h1>Projetos</h1>
         </div>
     )
