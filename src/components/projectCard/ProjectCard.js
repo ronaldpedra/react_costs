3816e79b9caedc842a2bf16782cef9ecf3232c1c
Ronald Pedra <ronaldpedra@gmail.com>
2024-06-13 22:01:40 -0300

    Update

--- src/components/projectCard/ProjectCard.js
+++ src/components/projectCard/ProjectCard.js
@@ -1,11 +1,20 @@
 import styles from "./ProjectCard.module.css"
 
 function ProjectCard({id, name, budget, category, handleRemove}){
+
+    function moeda(valor) {
+        var valorMoeda = Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(valor)
+        return valorMoeda
+    }
     return (
-        <div>
-            <p>{name}</p>
-            <p>{budget}</p>
-            <p>{category}</p>
+        <div className={styles.project_card_container}>
+            <h4>{name}</h4>
+            <p>
+                <span>Orçamento:</span> {moeda(budget)}
+            </p>
+            <p className={styles.category_text}>
+                <span className={`${styles[category.toLowerCase()]}`}></span>{category}
+            </p>
             <button>Editar</button>
             <button>Excluir</button>
         </div>
