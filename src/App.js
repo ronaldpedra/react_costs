4113fc721741ec9c7d10f50ec353cc7feddb2999
Ronald Pedra <ronaldpedra@gmail.com>
2024-06-15 18:38:10 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,24 +3,21 @@ import Main from "./components/layout/Main"
 import Footer from "./components/layout/Footer"
 import styles from "./css/App.module.css"
 import Container from "./components/layout/Container"
-import { Route, Router, Routes } from "react-router-dom"
+import { Router, Routes, Route } from "react-router-dom"
 import Home from "./components/pages/Home"
 
 export default function App() {
   return (
     <Router>
-      <div className={styles.app_container}>
-        <Header />
-        <Routes>
-          <Main>
-            <Container>
-              <Route path="/" element={<Home />} />
-            </Container>
-          </Main>
-        </Routes>
-        <Footer />
-      </div>
+      <Header />
+      <Main>
+        <Container>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Container>
+      </Main>
+      <Footer />
     </Router>
-
   )
 }
