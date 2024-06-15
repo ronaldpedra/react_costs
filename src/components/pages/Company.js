6d6d57e6bd96236d72df995113fa2c07834e6518
Ronald Pedra <ronaldpedra@gmail.com>
2024-06-15 16:08:03 -0300

    Cleaning

--- src/components/pages/Company.js
+++
@@ -1,9 +0,0 @@
-function Company() {
-    return (
-        <div>
-            <h1>Empresa</h1>
-        </div>
-    )
-}
-
-export default Company
--- src/components/pages/Contact.js
+++
@@ -1,9 +0,0 @@
-function Contact() {
-    return (
-        <div>
-            <h1>Contato</h1>
-        </div>
-    )
-}
-
-export default Contact
