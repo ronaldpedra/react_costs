b9b857786b9736503280af4446c9422f8e5d93f9
Ronald Pedra <ronaldpedra@gmail.com>
2024-06-16 15:49:41 -0300

    Update Container.js

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -2,7 +2,7 @@ import styles from "../../css/layout/Container.module.css"
 
 export default function Container(props) {
     return (
-        <div className={`${styles.container} ${styles[customClass]}`}>
+        <div className={`${styles.container} ${styles[props.customClass]}`}>
             {props.children}
         </div>
     )
