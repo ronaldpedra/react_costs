6b81aa9fe690810255ed0306795c6fb4fcf947c6
Ronald Pedra <ronaldpedra@gmail.com>
2024-06-16 13:39:16 -0300

    Update

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,15 @@
 import styles from "../../css/pages/Home.module.css"
+import LinkButton from "../single/LinkButton"
 
 export default function Home() {
     return (
-            <div className={styles.home_container}>
-                <h4>Bem-vindo ao Costs</h4>
-                <p>Crie, Planeje e Gerencie seus projetos aqui!!!</p>
-            </div>
+        <div className={styles.home_container}>
+            <h4>Bem-vindo ao Costs</h4>
+            <p>Crie, Planeje e Gerencie seus projetos aqui!!!</p>
+            <LinkButton
+                to={'/createproject'}
+                text={'Criar Projeto'}
+            />
+        </div>
     )
 }
--- src/components/single/LinkButton.js
+++ src/components/single/LinkButton.js
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
+import styles from "../../css/singleComponent/LinkButton.module.css"
 
 export default function LinkButton({ to, text }) {
     return (
-        <Link to={to}>{text}</Link>
+        <Link className={styles.btn} to={to}>{text}</Link>
     )
 }
