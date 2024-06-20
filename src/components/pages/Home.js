17fc2eb1185be4e760ff219ff591a9a5423cdd57
Ronald Pedra <ronaldpedra@gmail.com>
2024-06-19 22:36:59 -0300

    Update

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,16 @@
+import Container from "../layout/Container";
+import Savings from "../../img/savings.svg"
+import styles from "../../css/pages/Home.module.css"
 
 export default function Home() {
     return (
+        <Container customClass='center_column'>
+            <h1>Bem-Vindo ao <span>Costs</span></h1>
+            <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+            <button className={styles.btn}>Criar Projeto</button>
+            <img src={Savings} alt="Savings" />
+        </Container>
 
-        <h1>Home</h1>
 
 
     )
