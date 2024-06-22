d69675e10880e45b3efe4673fc874ec4e6eaecd2
Ronald Pedra <ronaldpedra@gmail.com>
2024-06-22 17:25:00 -0300

    Update

--- src/components/Single/Select.js
+++ src/components/Single/Select.js
@@ -1,11 +1,16 @@
 import styles from "../../css/single/Select.module.css"
 
-export default function Select({ label, name }) {
+export default function Select({ label, name, options }) {
+    console.log(options)
     return (
         <div className={styles.select_col}>
             <label htmlFor={name}>{label}</label>
             <select name={name} id={name}>
                 <option value="0">Selecione uma categoria</option>
+                {options &&
+                    options.map((option) => {
+                        <option value={option.id}>{option.name}</option>
+                    })}
             </select>
         </div>
     )
--- src/components/wraped/CreateProjectForm.js
+++ src/components/wraped/CreateProjectForm.js
@@ -3,19 +3,49 @@ import LinkButton from "../single/LinkButton";
 import Select from "../single/Select";
 import SubmitButton from "../single/SubmitButton";
 import styles from "../../css/wraped/Form.module.css";
+import { useEffect, useState } from "react";
 
 export default function CreateProjectForm() {
+
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/categories`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
     <form className={styles.form}>
       <fieldset>
         <legend>Dados do Projeto</legend>
         <div className={styles.row}>
           <div className={styles.col}>
-            <Input label={'Nome:'} type={'text'} name={'name'} placeholder={'Nome do Projeto'} />
-            <Input label={'Orçamento:'} type={'number'} name={'budget'} placeholder={'Orçamento do Projeto'} />
+            <Input
+              label={'Nome:'}
+              type={'text'}
+              name={'name'}
+              placeholder={'Nome do Projeto'}
+            />
+            <Input
+              label={'Orçamento:'}
+              type={'number'}
+              name={'budget'}
+              placeholder={'Orçamento do Projeto'}
+            />
             <Select
               label={'Categoria:'}
-              name={'category'} />
+              name={'category'}
+              options={categories}
+            />
             <div className={styles.actions}>
               <LinkButton to="/" btntext={"Cancelar"} />
               <SubmitButton btnText={'Criar Projeto'} />
